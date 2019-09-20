aea0b813eeb6cd9b6b37d5500d216197fdc0ba48
Saurabh Verma <saurabhshalu@gmail.com>
2019-09-20 20:08:20 +0530

    Fixed navigation bug!

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 function goToDiv(x) {
-    var topOfElement = document.querySelector(x).offsetTop - document.querySelector('header').offsetHeight;
-    window.scroll({ top: topOfElement, behavior: "smooth" });
+    // var el = document.querySelector(x);
+    // var rect = el.getBoundingClientRect(),
+    // scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+    // scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    // console.log('top: ' + rect.top + scrollTop);
+
+
+    // //console.log(document.querySelector(x).getBoundingClientRect().top);
+    // var topOfElement = document.querySelector(x).offsetTop - document.querySelector('header').offsetHeight;
+    // window.scrollTo(0,0);
+    // window.scroll({ top: topOfElement, behavior: "smooth" });
+    // console.log('1');
+
+    //var el = document.querySelector(x);
+    //el.scrollIntoView(true);
 }
